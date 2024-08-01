14302920df4e914d9b24b4b88052164399096f30
Hamzaabro5 <hamzaabro683@gmail.com>
2024-08-01 15:09:54 +0500

    Todo App

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-const input = document.querySelector(`input`)
-const addBtn = document.querySelector(`.addBtn`)
+
+// import {
+//     collection,
+//     addDoc,
+//     getDocs,
+
+// } 
+// from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+//  import {db} from "./config.js";
+
+const input = document.querySelector(`input`);
+const form = document.querySelector(`form`);
+const ul = document.querySelector(`ul`);
+const arr = [];
+
+function renderTodo() {
+    ul.innerHTML = "";
+    arr.push(input.value);
+    for (let i = 0; i < arr.length; i++) {
+        ul.innerHTML += `
+        <li>${arr[i]}</li>
+        `
+        input.value = ``
+    }
+}
+
+form.addEventListener(`submit` , async (event)=>{
+    event.preventDefault();
+    
+    renderTodo()
+})
+
 
